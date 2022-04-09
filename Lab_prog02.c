6974927803acf20deab3518af33cbb2819ffe771
1KS20AI001-Abhishek <94467192+1KS20AI001-Abhishek@users.noreply.github.com>
2022-04-09 16:10:58 +0530

    Update Lab_prog02.c

--- Lab_prog02.c
+++ Lab_prog02.c
@@ -1,48 +1,147 @@
 //DSA_LAB_PROGRAM_2
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
+//pati = pattern string index
+
+//compi = compare index
+
+//stri = main index
+
+//ansi = ans index
+
+//repi = replace index
 
 //finding pattern in string
+
 void findpat(char *str, char *pat, char *rep){
-    int i,m,k,c,j,flag=0;
+
+    int pati,compi,repi,stri,ansi,flag=0;
+
     char res[100];
-    i = m = c = j = 0;
-    while ( str[c]!='\0'){
-        if (str[m] == pat[i]){ 
-            i++;
-            m++;
-            if ( pat[i]=='\0'){
-                for(k=0; rep[k]!='\0'; k++){
-                    res[j] = rep[k];
-                    j++;
+
+    pati = compi = stri = ansi = 0;
+
+    while ( str[stri]!='\0'){
+
+        if (str[compi] == pat[pati]){ 
+
+            pati++;
+
+            compi++;
+
+            if ( pat[pati]=='\0'){
+
+                for(repi=0; rep[repi]!='\0'; repi++){
+
+                    res[ansi] = rep[repi];
+
+                    ansi++;
+
                     flag=1;
+
                 }
-                i=0;
-                c=m;
+
+                pati=0;
+
+                stri=compi;
+
             }
+
         }
+
         else{
-            res[j] = str[c];
-            j++;
-            c++;
-            m = c;
-            i=0;
+
+            res[ansi] = str[stri];
+
+            ansi++;
+
+            stri++;
+
+            compi = stri;
+
+            pati=0;
+
         }
+
     }
+
     if(flag==0){
+
         printf("pattern not found\n");
+
     }
+
     else{
-        res[j] = '\0';
+
+        res[ansi] = '\0';
+
         printf("\nThe resultant string is: %s\n" , res);
+
     }
+
 }
 
 int main(){
+
     char str[100], pat[100], rep[100];
-    scanf("%s %s %s", &str, &pat, &rep);
+
+    scanf("%s %s %s", str, pat, rep);
+
     findpat(str, pat, rep);
+
     return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
